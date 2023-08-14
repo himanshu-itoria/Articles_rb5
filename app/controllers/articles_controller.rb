cf3f7cb202b4258680b5b65abf1ef59b3583665e
itoriahimanshu <itoriahimanshu@gmail.com>
2023-08-14 19:10:24 +0530

    Saving the new entries in artices form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,15 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        @article = Article.new
+        @article1 = Article.new
     end
 
     def create
+        @article1 = Article.new(params.require(:article).permit(:title,:description))
+        @article1.save
+        # redirect_back(fallback_location: @article)
+        # redirect_to action:show,id:
+        redirect_to @article1
     end
 
 
