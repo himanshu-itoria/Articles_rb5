67fc507a1275ad9c38fe50499d6334745ecd3e1b
itoriahimanshu <itoriahimanshu@gmail.com>
2023-08-14 11:34:45 +0530

    Creating the articles blog webapp

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#hello'
+
+    get 'about', to: 'home#about'
+
+end
+ 
+#         1. Create an alpha-blog application.
+
+# 2. Setup tracking with Git and make a commit of your code with the message "Initial commit". Set up a GitHub repository for the app.
+
+# 3. Create a home page view (as the root route) and an about page like the test app.
+
+# 4. Make sure you are able to start the server and preview the application. Take snapshots of the two pages (home and about) and post them to the Q & A.
+
+# 5. Make a commit of your code and push to your GitHub repository for the application.
